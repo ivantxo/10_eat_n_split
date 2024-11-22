8369b9786cf44a85f50d66bcda980709e1005cee
IvanR <ivan.ramirezf@gmail.com>
2024-11-23 00:32:40 +1000

    Make AddFriend form inputs controlled elements

--- src/FormAddFriend.js
+++ src/FormAddFriend.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import Button from "./Button";
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
   return (
     <form className="form-add-friend">
       <label>🧑‍🤝‍🧑Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🌞 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
