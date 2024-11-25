7f46027338299cb89442c770ddbf8aadbb6f905b
IvanR <ivan.ramirezf@gmail.com>
2024-11-25 22:45:15 +1000

    Validate that user expense
    - It needs to be less or equal than bill value

--- src/FormSplitBill.js
+++ src/FormSplitBill.js
@@ -22,7 +22,11 @@ function FormSplitBill({ selectedFriend }) {
       <input
         type="text"
         value={paidByUser}
-        onChange={(e) => setPaidByUser(Number(e.target.value))}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
       />
 
       <label>🧑‍🤝‍🧑 {selectedFriend.name}'s expense</label>
